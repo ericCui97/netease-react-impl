2c9d01dbf368d42caa5773d19c8fe3509c0443ee
cui <cui@123.com>
2020-09-04 04:06:25 +0800

    rank 完成

--- src/api/request.js
+++ src/api/request.js
@@ -15,3 +15,6 @@ export const getSingerListRequest = (category, alpha, count) => {
     `/artist/list?cat=${category}&initial=${alpha.toLowerCase()}&offset=${count}`
   );
 };
+export const getRankListRequest = () => {
+    return axiosInstance.get (`/toplist/detail`);
+  };
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux-immutable";
 import { reducer as recommendReducer } from "../application/Recommend/store/index";
 import { reducer as singersReducer } from "../application/Singers/store/index";
+import { reducer as rankReducer } from "../application/Rank/store/index";
 
 export default combineReducers({
+  rank: rankReducer,
   recommend: recommendReducer,
   singers: singersReducer,
 });
